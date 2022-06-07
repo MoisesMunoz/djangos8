3aa805beb6c3095f7bf2e9eab89070d29924e8b7
Mauricio Astudillo <m.astudillo@duocuc.cl>
2022-06-07 15:46:22 -0400

    Arreglo de pagina y creacion de urls y views

--- srv/mascota/urls.py
+++ srv/mascota/urls.py
@@ -2,7 +2,7 @@ from importlib.resources import path
 from unicodedata import name
 
 
-from mascota.views import index, login, productogato, productoperro, sobrenosotros
+from mascota.views import index, login, productogato, productoperro, sobrenosotros, perro1, perro2, perro3, gato1, gato2, gato3, carro, adminañadymod, admin, conf_pagar
 from django.urls import path
 
 urlpatterns = [
@@ -10,5 +10,16 @@ urlpatterns = [
     path('sobrenosotros',sobrenosotros,name="sobrenosotros"),
     path('productoperro',productoperro,name="productoperro"),
     path('productogato',productogato,name="productogato"),
-    path('login',login,name="login")
+    path('login',login,name="login"),
+    path('perro1',perro1,name="perro1"),
+    path('perro2',perro2,name="perro2"),
+    path('perro3',perro3,name="perro3"),
+    path('gato1',gato1,name="gato1"),
+    path('gato2',gato2,name="gato2"),
+    path('gato3',gato3,name="gato3"),
+    path('carro',carro,name="carro"),
+    path('admin',admin,name="admin"),
+    path('adminañadymod',adminañadymod,name="adminañadymod"),
+    path('conf_pagar',conf_pagar,name="conf_pagar")
+    
 ]
