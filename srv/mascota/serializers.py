ff44f7514221277b2c503b9f2cd1479a82c74404
Mauricio <m.astudillo@duocuc.cl>
2022-07-01 01:47:54 -0400

    producto sin s

--- srv/mascota/serializers.py
+++ srv/mascota/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Productos
+from core.models import Producto
 
 class Productoserializer(serializers.ModelSerializer):
     class Meta:
